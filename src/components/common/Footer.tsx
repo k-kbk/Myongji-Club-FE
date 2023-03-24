581596052f84398c479e0f0825c5b9f3484c7533
bk <kkbk0077@gmail.com>
2023-03-24 22:07:39 +0900

    feat: 푸터 구현

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="flex w-full max-w-screen-lg flex-col py-12 px-8 md:px-6">
+      <div className="text-xs font-medium text-gray-500 md:text-sm">
+        Copyright ⓒ Myongji-Club. All Rights Reserved
+      </div>
+      <div className="text-xs text-gray-400 md:text-sm">
+        이메일: myongjiuniv.club@gmail.com
+      </div>
+    </footer>
+  );
+}
