617463dc4ee91300afe8eb671265b32a9ff883af
bk <kkbk0077@gmail.com>
2023-03-24 22:07:59 +0900

    refactor: 레이아웃 수정

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,16 @@
 import Header from '../common/Header';
+import Footer from '../common/Footer';
 
-interface LayoutProps {
+type LayoutProps = {
   children: React.ReactNode;
-}
+};
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-start bg-white">
+    <div className="flex min-h-screen w-full flex-col items-center justify-between">
       <Header />
       {children}
+      <Footer />
     </div>
   );
 }
