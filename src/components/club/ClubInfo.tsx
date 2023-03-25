58dad571f68a0e8d37ebf9fde2ede770373bd3b0
bk <kkbk0077@gmail.com>
2023-03-26 02:22:04 +0900

    design: 기타 디자인 수정

--- src/components/club/ClubInfo.tsx
+++ src/components/club/ClubInfo.tsx
@@ -1,28 +1,38 @@
 export default function ClubInfo() {
   return (
     <div className="mt-4 rounded-lg border border-gray-100 bg-gray-50 px-6 py-4 text-base font-semibold sm:mt-6 sm:px-8 sm:py-6 sm:text-lg">
-      <div className="sm:mb-1">
-        <span className="inline-block w-52">
-          <span className="mr-3 text-gray-400">회장</span>
-          <span className="">박수환</span>
+      <div>
+        <span className="mb-0.5 inline-block w-52">
+          <span className="mr-3 inline-block w-14 text-gray-400 sm:w-18">
+            회장
+          </span>
+          <span>박수환</span>
         </span>
-        <span className="inline-block">
-          <span className="mr-3 text-gray-400">연락처</span>
+        <span className="mb-0.5 inline-block">
+          <span className="mr-3 inline-block w-14 text-gray-400 sm:w-18">
+            연락처
+          </span>
           <span>010-0807-4328</span>
         </span>
       </div>
-      <div className="sm:mb-1">
-        <span className="inline-block w-52">
-          <span className="mr-3 text-gray-400">동아리방</span>
+      <div>
+        <span className="mb-0.5 inline-block w-52">
+          <span className="mr-3 inline-block w-14 text-gray-400 sm:w-18">
+            동아리방
+          </span>
           <span>S4018</span>
         </span>
-        <span className="inline-block">
-          <span className="mr-3 text-gray-400">정기모임</span>
+        <span className="mb-0.5 inline-block">
+          <span className="mr-3 inline-block w-14 text-gray-400 sm:w-18">
+            정기모임
+          </span>
           <span>매주 수요일 18시</span>
         </span>
       </div>
       <div>
-        <span className="mr-3 text-gray-400">모집기간</span>
+        <span className="mr-3 inline-block w-14 text-gray-400 sm:w-18">
+          모집기간
+        </span>
         <span>3월 20일 ~ 3월 31일</span>
       </div>
     </div>
