5769c6c539483bab6314fe915b85e9e6ac89ee03
bk <kkbk0077@gmail.com>
2023-03-26 01:09:58 +0900

    feat: 일러스트 출처 표기 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,8 +4,16 @@ export default function Footer() {
       <div className="text-xs font-medium text-gray-500 md:text-sm">
         Copyright ⓒ Myongji-Club. All Rights Reserved
       </div>
-      <div className="text-xs text-gray-400 md:text-sm">
+      <div className="mb-2 text-xs text-gray-400 md:text-sm">
         이메일: myongjiuniv.club@gmail.com
+      </div>
+      <div className="text-xs text-gray-400">
+        <span>Illustration by </span>
+        <a href="https://iconscout.com/contributors/iconscout">
+          IconScout Store
+        </a>
+        <span> on </span>
+        <a href="https://iconscout.com">IconScout</a>
       </div>
     </footer>
   );
