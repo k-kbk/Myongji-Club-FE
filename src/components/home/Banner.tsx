de815bbccc7a51e767b2ee6d42ae5c60eee676b8
bk <kkbk0077@gmail.com>
2023-03-25 12:18:38 +0900

    design: 배너 이미지 수정

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -4,15 +4,15 @@ export default function Banner() {
   return (
     <div className="flex h-56 w-full flex-col items-center justify-center rounded-lg bg-indigo-100 px-8 text-center shadow-sm md:h-48 md:flex-row md:px-0 md:text-left">
       <Image
-        src="hi.svg"
-        width={180}
-        height={180}
+        src="hand.svg"
+        width={140}
+        height={140}
         quality={100}
         alt="banner"
         priority
-        className="mb-2 h-24 md:mx-0 md:h-30"
+        className="mb-2 h-24 md:mb-0 md:h-36"
       />
-      <div className="break-keep md:mx-8">
+      <div className="mb-2 break-keep md:mx-4 md:mb-0">
         <p className="text-2xl font-bold text-gray-900 md:text-4xl">
           명지클럽이 탄생했어요!
         </p>
