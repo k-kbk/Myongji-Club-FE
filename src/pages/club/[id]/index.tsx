f422cb92672f4852c82261d45962eea22b0e8e40
bk <kkbk0077@gmail.com>
2023-03-26 00:16:35 +0900

    feat: 이전으로 돌아가기 버튼 추가

--- src/pages/club/[id]/index.tsx
+++ src/pages/club/[id]/index.tsx
@@ -2,6 +2,7 @@ import { DeptCaptionType } from '@/types/type';
 import { GetServerSideProps } from 'next/types';
 import ClubInfo from '@/components/club/ClubInfo';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const dummy = [
   {
@@ -256,46 +257,56 @@ export default function Index({ clubId }: { clubId: number }) {
   const item = dummy[clubId - 1];
 
   return (
-    <div className="mt-28 w-full rounded-lg bg-white px-4 py-6 shadow-sm sm:mb-14 sm:py-14 sm:px-12">
-      <h1 className="mr-3 inline text-4xl font-bold sm:mr-6 sm:text-4.5xl">
-        {item.name}
-      </h1>
-      <span
-        className={`mr-1 rounded-lg px-2 py-1 text-sm font-semibold shadow-sm sm:text-base ${
-          deptCaption[item.category]
-        }`}
-      >
-        {item.category}
-      </span>
-      <span className="ml-1 rounded-lg bg-gray-100 px-2 py-1 text-sm font-semibold text-gray-500 shadow-sm sm:text-base">
-        {item.tag}
-      </span>
-      <ClubInfo />
-      <div className="mt-8 text-start text-lg font-bold sm:mt-12 sm:text-xl">
-        대표사진
+    <>
+      <div className="mt-28 mb-8 w-full rounded-lg bg-white px-4 py-6 shadow-sm sm:py-14 sm:px-12">
+        <h1 className="mr-3 inline text-3.5xl font-bold sm:mr-6 sm:text-4.5xl">
+          {item.name}
+        </h1>
+        <span
+          className={`mr-1 rounded-lg px-2 py-1 text-sm font-semibold shadow-sm sm:text-base ${
+            deptCaption[item.category]
+          }`}
+        >
+          {item.category}
+        </span>
+        <span className="ml-1 rounded-lg bg-gray-100 px-2 py-1 text-sm font-semibold text-gray-500 shadow-sm sm:text-base">
+          {item.tag}
+        </span>
+        <ClubInfo />
+        <div className="mt-8 text-start text-lg font-bold sm:mt-12 sm:text-xl">
+          대표사진
+        </div>
+        <div className="mt-2 flex flex-col items-center justify-center sm:mt-3">
+          <Image
+            src="/default.svg"
+            width={1000}
+            height={1000}
+            priority
+            alt="default"
+            className="w-full rounded-lg border border-gray-100"
+          />
+        </div>
+        <div className="mt-8 text-start text-lg font-bold sm:mt-12 sm:text-xl">
+          동아리 소개
+        </div>
+        <div className="mt-2 sm:mt-3">
+          {content.map((line, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <p key={index} className="text-base font-medium sm:text-lg">
+              {line}
+            </p>
+          ))}
+        </div>
       </div>
-      <div className="mt-2 flex flex-col items-center justify-center sm:mt-3">
-        <Image
-          src="/default.svg"
-          width={1000}
-          height={1000}
-          priority
-          alt="default"
-          className="w-full rounded-lg border border-gray-100"
-        />
+      <div className="mb-14 flex justify-end">
+        <Link
+          href="/"
+          className="rounded-lg bg-gray-200 bg-opacity-50 px-4 py-2.5 text-sm font-semibold text-gray-500 drop-shadow-sm transition-opacity hover:opacity-50 md:text-base"
+        >
+          이전으로 돌아가기
+        </Link>
       </div>
-      <div className="mt-8 text-start text-lg font-bold sm:mt-12 sm:text-xl">
-        동아리 소개
-      </div>
-      <div className="mt-2 sm:mt-3">
-        {content.map((line, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <p key={index} className="text-base font-medium sm:text-lg">
-            {line}
-          </p>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
