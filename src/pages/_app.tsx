270b297b1ec947c048e3def66e5b029a3012cf7a
bk <kkbk0077@gmail.com>
2023-03-26 12:16:27 +0900

    feat: 파비콘, 설명 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,10 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no"
         />
+        <link rel="icon" href="/favicon.svg" />
+        <link rel="mask-icon" href="/favicon.svg" color="#FFFFFF" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <meta name="description" content="명지대학교 동아리의 모든 것" />
         <title>명지클럽</title>
       </Head>
       <Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
-      <link
-        rel="stylesheet"
-        as="style"
-        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css"
-      />
+      <Head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
