fed44a832091d61b65d7a38557ee39ccf92543ae
bk <kkbk0077@gmail.com>
2023-03-27 10:15:40 +0900

    refactor: 어드민 페이지 헤더 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,11 +2,39 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+const navItems = {
+  user: [
+    {
+      id: 1,
+      name: '홈',
+      href: '/',
+    },
+    {
+      id: 2,
+      name: '공지사항',
+      href: '/notice',
+    },
+  ],
+  admin: [
+    {
+      id: 11,
+      name: '동아리',
+      href: '/admin/club',
+    },
+    {
+      id: 12,
+      name: '공지사항',
+      href: '/admin/notice',
+    },
+  ],
+};
+
 export default function Header() {
   const router = useRouter();
   /** 현재 경로 */
   const curPath = router.pathname;
-  const isAdmin = curPath.startsWith('/admin');
+  const isAdmin = curPath.startsWith('/admin') ? 'admin' : 'user';
+  const isAdminLogin = curPath === '/admin/login';
 
   return (
     <header className="fixed z-10 flex h-[3.75rem] w-full items-center justify-center bg-white px-3 shadow-sm sm:h-16 sm:px-4">
@@ -22,26 +50,22 @@ export default function Header() {
             className="w-32 sm:w-36"
           />
         </Link>
-        {!isAdmin && (
-          <div>
-            <Link
-              href="/"
-              className={`rounded-lg px-2.5 py-4 font-semibold sm:px-3 ${
-                curPath === '/' ? 'text-indigo-700' : 'text-gray-600'
-              } mx-1 transition-colors hover:text-indigo-700`}
-            >
-              홈
-            </Link>
-            <Link
-              href="/notice"
-              className={`rounded-lg px-2.5 py-4 font-semibold sm:px-3 ${
-                curPath.includes('notice') ? 'text-indigo-700' : 'text-gray-600'
-              } mx-1 transition-colors hover:text-indigo-700`}
-            >
-              공지사항
-            </Link>
-          </div>
-        )}
+        <nav>
+          {navItems[isAdmin].map(
+            (item) =>
+              !isAdminLogin && (
+                <Link
+                  key={item.id}
+                  href={item.href}
+                  className={`rounded-lg px-2.5 py-4 font-semibold sm:px-3 ${
+                    curPath === item.href ? 'text-indigo-700' : 'text-gray-600'
+                  } mx-1 transition-colors hover:text-indigo-700`}
+                >
+                  {item.name}
+                </Link>
+              ),
+          )}
+        </nav>
       </div>
     </header>
   );
