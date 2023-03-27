819fb695693e6053f4201259f8e3784184d9efcb
bk <kkbk0077@gmail.com>
2023-03-28 00:36:45 +0900

    design: 폰트 크기 수정

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -244,7 +244,7 @@ export default function Index() {
       <div className="mt-24 mb-8 sm:mt-30">
         {/* <h1 className="text-3xl font-bold sm:text-4xl">동아리 관리</h1> */}
         <div className="flex flex-col items-center justify-between border-y py-8 sm:flex-row sm:py-10">
-          <div className="flex flex-col items-center text-1.5xl font-bold sm:flex-row sm:text-3xl">
+          <div className="flex flex-col items-center text-1.5xl font-bold sm:flex-row sm:text-2.5xl">
             <div>안녕하세요,</div>
             <div className="sm:ml-2">총동아리연합회 님!</div>
           </div>
