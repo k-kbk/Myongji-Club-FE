9f52618b29728ec3e2710862f8c8c6017d55d4a1
bk <kkbk0077@gmail.com>
2023-03-29 14:30:52 +0900

    feat: 모바일 네비게이션 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import isSamePath from '@/utils/isSamePath';
+import MobileNav from './MobileNav';
 
 const navItems = {
   user: [
@@ -40,37 +41,37 @@ export default function Header() {
   const router = useRouter();
   /** 현재 경로 */
   const curPath = router.pathname;
-  const isAdmin = curPath.startsWith('/admin') ? 'admin' : 'user';
-  const isAdminLogin = curPath === '/admin/login';
+  const pathStartsWith = curPath.startsWith('/admin') ? 'admin' : 'user';
+  const isPathAdminLogin = curPath === '/admin/login';
 
   const [isClicked, setIsClicked] = useState<boolean>(false);
 
-  function handleClick() {
+  function handleMenuOpen() {
     setIsClicked((prev) => !prev);
   }
 
   return (
-    <header className="fixed z-10 flex h-[3.75rem] w-full items-center justify-center bg-white px-3 shadow-sm sm:h-16 sm:px-4">
-      <div className="flex w-full max-w-screen-lg items-center justify-between">
-        <Link href={navItems[isAdmin][0].href} className="px-3">
-          <Image
-            src="/logo_main.png"
-            width={800}
-            height={180}
-            quality={100}
-            priority
-            alt="myongji-club"
-            className="w-32 sm:w-36"
-          />
-        </Link>
-        {isClicked ? (
+    <>
+      <header className="fixed z-10 flex h-[3.75rem] w-full items-center justify-center bg-white px-3 shadow-sm sm:h-16 sm:px-4">
+        <div className="flex w-full max-w-screen-lg items-center justify-between">
+          <Link href={navItems[pathStartsWith][0].href} className="px-3">
+            <Image
+              src="/logo_main.png"
+              width={800}
+              height={180}
+              quality={100}
+              priority
+              alt="myongji-club"
+              className="w-32 sm:w-36"
+            />
+          </Link>
           <button
             type="button"
             className="block px-3 py-4 opacity-70 sm:hidden"
-            onClick={handleClick}
+            onClick={handleMenuOpen}
           >
             <Image
-              src="/close.png"
+              src={isClicked ? '/close.png' : '/menu.png'}
               width={96}
               height={96}
               quality={100}
@@ -79,42 +80,34 @@ export default function Header() {
               className="w-7"
             />
           </button>
-        ) : (
-          <button
-            type="button"
-            className="block px-3 py-4 opacity-70 sm:hidden"
-            onClick={handleClick}
-          >
-            <Image
-              src="/menu.png"
-              width={95}
-              height={96}
-              quality={100}
-              priority
-              alt="menu"
-              className="w-7"
-            />
-          </button>
-        )}
-        <nav className="hidden font-semibold sm:block sm:text-lg">
-          {navItems[isAdmin].map(
-            (item) =>
-              !isAdminLogin && (
-                <Link
-                  key={item.id}
-                  href={item.href}
-                  className={`rounded-lg px-2.5 py-4 sm:px-3 ${
-                    isSamePath(curPath, item.href)
-                      ? 'text-blue-600'
-                      : 'text-gray-600'
-                  } mx-1 transition-colors hover:text-blue-600`}
-                >
-                  {item.name}
-                </Link>
-              ),
-          )}
-        </nav>
-      </div>
-    </header>
+          <nav className="hidden font-semibold sm:block sm:text-lg">
+            {navItems[pathStartsWith].map(
+              (item) =>
+                !isPathAdminLogin && (
+                  <Link
+                    key={item.id}
+                    href={item.href}
+                    className={`rounded-lg px-2.5 py-4 sm:px-3 ${
+                      isSamePath(curPath, item.href)
+                        ? 'text-blue-600'
+                        : 'text-gray-600'
+                    } mx-1 transition-colors hover:text-blue-600`}
+                  >
+                    {item.name}
+                  </Link>
+                ),
+            )}
+          </nav>
+        </div>
+      </header>
+      {isClicked && (
+        <MobileNav
+          router={router}
+          pathStartsWith={pathStartsWith}
+          isPathAdminLogin={isPathAdminLogin}
+          setIsClicked={setIsClicked}
+        />
+      )}
+    </>
   );
 }
---
+++ src/components/common/MobileNav.tsx
@@ -0,0 +1,82 @@
+import isSamePath from '@/utils/isSamePath';
+import { NextRouter } from 'next/router';
+import { Dispatch, SetStateAction } from 'react';
+
+type NavItemsType = {
+  [index: string]: Array<{ id: number; name: string; href: string }>;
+};
+
+const navItems: NavItemsType = {
+  user: [
+    {
+      id: 1,
+      name: '홈',
+      href: '/',
+    },
+    {
+      id: 2,
+      name: '공지사항',
+      href: '/notice',
+    },
+  ],
+  admin: [
+    {
+      id: 11,
+      name: '홈',
+      href: '/admin',
+    },
+    {
+      id: 12,
+      name: '동아리 관리',
+      href: '/admin/club',
+    },
+    {
+      id: 13,
+      name: '공지사항 관리',
+      href: '/admin/notice',
+    },
+  ],
+};
+
+type MobileNavType = {
+  router: NextRouter;
+  pathStartsWith: string;
+  isPathAdminLogin: boolean;
+  setIsClicked: Dispatch<SetStateAction<boolean>>;
+};
+
+export default function MobileNav({
+  router,
+  pathStartsWith,
+  isPathAdminLogin,
+  setIsClicked,
+}: MobileNavType) {
+  const curPath = router.pathname;
+
+  function handleMenuClick(href: string) {
+    router.push(href);
+    setIsClicked((prev) => !prev);
+  }
+
+  return (
+    <nav className="fixed z-10 mt-[3.75rem] flex w-full flex-col border-t bg-white font-semibold shadow sm:hidden">
+      {navItems[pathStartsWith].map(
+        (item) =>
+          !isPathAdminLogin && (
+            <button
+              key={item.id}
+              type="button"
+              onClick={() => handleMenuClick(item.href)}
+              className={`border-gray-100 px-6 py-5 text-left sm:px-3 ${
+                isSamePath(curPath, item.href)
+                  ? 'text-blue-600'
+                  : 'text-gray-600'
+              } transition-colors hover:text-blue-600`}
+            >
+              {item.name}
+            </button>
+          ),
+      )}
+    </nav>
+  );
+}
